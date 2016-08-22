5206104a1f9cfdb1457934fa87c26ed5da62a86b
Juri <juri.strumpflohner@gmail.com>
2016-08-22 21:44:54 +0200

    ..

--- src/app/graph/graph.component.ts
+++ src/app/graph/graph.component.ts
@@ -3,215 +3,231 @@ import { NgZone } from '@angular/core';
 import * as d3 from 'd3';
 
 @Component({
-    selector: 'code-graph',
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    template: ''
+  selector: 'code-graph',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: ''
 })
 export class CodeGraphComponent implements OnInit, OnChanges {
-    private width: number = 1200;
-    private height: number = 800;
-    private svg: any;
-    private g: any;
-    private linksContainer: any;
-    private nodesContainer: any;
-    private color: any;
-    private simulation: any;
-    private link: any;
-    private node: any;
-    private tooltip: any;
-    private is2ndUpdate: boolean = false;
+  private width: number = 1200;
+  private height: number = 800;
+  private svg: any;
+  private g: any;
+  private linksContainer: any;
+  private nodesContainer: any;
+  private color: any;
+  private simulation: any;
+  private link: any;
+  private node: any;
+  private tooltip: any;
+  private is2ndUpdate: boolean = false;
 
-    @Input() graphData: any;
+  @Input() graphData: any;
 
-    constructor(private element: ElementRef, private zone: NgZone) {
+  constructor(private element: ElementRef, private zone: NgZone) {
+  }
+
+  ngOnChanges(changes: { [propName: string]: SimpleChange }) {
+    this.graphData = changes['graphData'].currentValue;
+    console.log('New data', this.graphData);
+
+    if (this.graphData) {
+      // if (this.is2ndUpdate) {
+      //     this.updateGraph(this.graphData);
+      // } else {
+      this.render(this.graphData);
+      // }
+      // this.is2ndUpdate = true;
     }
+  }
 
-    ngOnChanges(changes: { [propName: string]: SimpleChange }) {
-        this.graphData = changes['graphData'].currentValue;
-        console.log('New data', this.graphData);
+  ngOnInit() {
 
-        if (this.graphData) {
-            // if (this.is2ndUpdate) {
-            //     this.updateGraph(this.graphData);
-            // } else {
-            this.render(this.graphData);
-            // }
-            // this.is2ndUpdate = true;
+    this.tooltip = d3.select(this.element.nativeElement)
+      .append('div')
+      .attr('class', 'tooltip')
+      .style('opacity', 0);
+
+    this.svg = d3.select(this.element.nativeElement)
+      .append('svg')
+      .attr('width', this.width)
+      .attr('height', this.height);
+
+    this.g = this.svg.append('g');
+
+    this.linksContainer = this.g.append('g').attr('class', 'links-container');
+    this.nodesContainer = this.g.append('g').attr('class', 'nodes-container');
+
+    this.svg
+      .style("pointer-events", "all")
+      .call(d3.zoom()
+        .on("zoom", () => this.g.attr("transform", d3.event.transform)));
+
+    this.color = d3.scaleOrdinal(d3.schemeCategory20);
+
+    this.simulation = d3.forceSimulation()
+      .force("link", d3.forceLink().id(function (d) { return d.id; }))
+      .force("charge", d3.forceManyBody())
+      .force("center", d3.forceCenter(this.width / 2, this.height / 2));
+  }
+
+  render(graph) {
+    this.zone.runOutsideAngular(() => {
+
+      let markers = this.svg.append("defs")
+        .selectAll("marker")
+        .data(['regular']);
+
+      let newMarkers = markers
+        .enter()
+        .append("svg:marker")
+        .attr("id", String)
+        .attr("viewBox", "0 -5 10 10")
+        .attr("refX", 23)
+        .attr("refY", 0)
+        .attr("markerWidth", 6)
+        .attr("markerHeight", 6)
+        .attr("orient", "auto")
+        .append("path")
+        .attr("d", "M0,-5L10,0L0,5");
+
+      // create the links container and fill with data
+      var link = this.linksContainer
+        .selectAll("line")
+        .data(graph.links);
+
+      link.attr('class', (d) => {
+        if (d.highlighted) {
+          console.log('graph: is highlighted', d);
+          return 'link highlighted';
+        } else {
+          return 'link';
         }
-    }
+      });
 
-    ngOnInit() {
+      // add new links
+      var newLinks = link
+        .enter()
+        .append("line")
+        .attr('class', (d) => {
+          if (d.highlighted) {
+            console.log('graph: is highlighted', d);
+            return 'link highlighted';
+          } else {
+            return 'link';
+          }
+        })
+        .attr("stroke-width", (d) => Math.sqrt(d.value))
+        .attr("data-target", function (o) { return o.target })
+        .attr("data-source", function (o) { return o.source })
+        .attr("marker-end", function (d) { return "url(#regular)"; });
 
-        this.tooltip = d3.select(this.element.nativeElement)
-            .append('div')
-            .attr('class', 'tooltip')
-            .style('opacity', 0);
+      // remove old ones
+      var exitLinks = link.exit().remove();
 
-        this.svg = d3.select(this.element.nativeElement)
-            .append('svg')
-            .attr('width', this.width)
-            .attr('height', this.height);
-
-        this.g = this.svg.append('g');
-
-        this.linksContainer = this.g.append('g').attr('class', 'links-container');
-        this.nodesContainer = this.g.append('g').attr('class', 'nodes-container');
-
-        this.svg
-            .style("pointer-events", "all")
-            .call(d3.zoom()
-                .on("zoom", () => this.g.attr("transform", d3.event.transform));
-
-        this.color = d3.scaleOrdinal(d3.schemeCategory20);
-
-        this.simulation = d3.forceSimulation()
-            .force("link", d3.forceLink().id(function (d) { return d.id; }))
-            .force("charge", d3.forceManyBody())
-            .force("center", d3.forceCenter(this.width / 2, this.height / 2));
-    }
-
-    render(graph) {
-        this.zone.runOutsideAngular(() => {
-
-            let markers = this.svg.append("defs")
-                .selectAll("marker")
-                .data(['regular']);
-
-            let newMarkers = markers
-                .enter()
-                .append("svg:marker")
-                .attr("id", String)
-                .attr("viewBox", "0 -5 10 10")
-                .attr("refX", 23)
-                .attr("refY", 0)
-                .attr("markerWidth", 6)
-                .attr("markerHeight", 6)
-                .attr("orient", "auto")
-                .append("path")
-                .attr("d", "M0,-5L10,0L0,5");
-
-            // create the links container and fill with data
-            var link = this.linksContainer
-                .selectAll("line")
-                .data(graph.links);
-
-            // add new links
-            var newLinks = link
-                .enter()
-                .append("line")
-                .attr('class', 'link')
-                .attr("stroke-width", (d) => Math.sqrt(d.value))
-                .attr("data-target", function(o) { return o.target })
-                .attr("data-source", function(o) { return o.source })
-                .attr("marker-end", function(d) { return "url(#regular)"; });
-
-            // remove old ones
-            var exitLinks = link.exit().remove();
-
-            // merge changes
-            link = newLinks.merge(link);
+      // merge changes
+      link = newLinks.merge(link);
 
 
 
-            var self = this;
-            // create nodes container and fill with data
-            var node = this.nodesContainer
-                .selectAll("circle")
-                .data(graph.nodes);
+      var self = this;
+      // create nodes container and fill with data
+      var node = this.nodesContainer
+        .selectAll("circle")
+        .data(graph.nodes);
 
-            // update the nodes attributes based
-            // on the new data
-            node
-                .attr("fill", (d) => {
-                    if (d.isHighlighted) {
-                        console.log('coloring highlighted node', d);
-                        return '#7CFC00';
-                    } else {
-                        return this.color(1);
-                    }
-                });
-
-            // add eventual new nodes
-            var newNodes = node
-                .enter().append("circle")
-                .attr('class', 'node')
-                .attr("r", (d) => d.weight * 10 || 10)
-                .attr("fill", (d) => {
-                    if (d.isHighlighted) {
-                        console.log('coloring highlighted node', d);
-                        return '#7CFC00';
-                    } else {
-                        return this.color(1);
-                    }
-                })
-                .on('mouseover', (d) => {
-                    this.tooltip
-                        .transition()
-                        .duration(500)
-                        .style('opacity', .85);
-
-                    this.tooltip.html(d.id)
-                        .style('left', (d3.event.pageX) + 'px')
-                        .style('top', (d3.event.pageY - 28) + 'px');
-                })
-                .on('mouseout', (d) => {
-                    this.tooltip.transition()
-                        .duration(300)
-                        .style('opacity', 0);
-                })
-                .on('dblclick', function (d) {
-                    d.fx = null;
-                    d.fy = null;
-
-                    d3.select(this).classed('fixed', false);
-                })
-                .call(d3.drag()
-                    .on("start", function (d) {
-                        if (!d3.event.active) self.simulation.alphaTarget(0.3).restart();
-                        d.fx = d.x;
-                        d.fy = d.y;
-                    })
-                    .on("drag", function (d) {
-                        d.fx = d3.event.x;
-                        d.fy = d3.event.y;
-                    })
-                    .on("end", function (d) {
-                        if (!d3.event.active) self.simulation.alphaTarget(0);
-                        // don't reset the fx and fy coordinates to have the node stick at that position
-                        // d.fx = null;
-                        // d.fy = null;
-
-                        d3.select(this).classed('fixed', true);
-                    })
-                );
-
-            // remove old nodes
-            node.exit().remove();
-
-            node = newNodes.merge(node);
-
-
-            this.simulation
-                .nodes(graph.nodes)
-                .on("tick", ticked);
-
-            this.simulation.force("link")
-                .links(graph.links);
-
-            ticked();
-
-            function ticked() {
-                link
-                    .attr("x1", function (d) { return d.source.x; })
-                    .attr("y1", function (d) { return d.source.y; })
-                    .attr("x2", function (d) { return d.target.x; })
-                    .attr("y2", function (d) { return d.target.y; });
-
-                node
-                    .attr("cx", function (d) { return d.x; })
-                    .attr("cy", function (d) { return d.y; });
-            }
+      // update the nodes attributes based
+      // on the new data
+      node
+        .attr("fill", (d) => {
+          if (d.isHighlighted) {
+            console.log('coloring highlighted node', d);
+            return '#7CFC00';
+          } else {
+            return this.color(1);
+          }
         });
-    }
+
+      // add eventual new nodes
+      var newNodes = node
+        .enter().append("circle")
+        .attr('class', 'node')
+        .attr("r", (d) => d.weight * 10 || 10)
+        .attr("fill", (d) => {
+          if (d.isHighlighted) {
+            console.log('coloring highlighted node', d);
+            return '#7CFC00';
+          } else {
+            return this.color(1);
+          }
+        })
+        .on('mouseover', (d) => {
+          this.tooltip
+            .transition()
+            .duration(500)
+            .style('opacity', .85);
+
+          this.tooltip.html(d.id)
+            .style('left', (d3.event.pageX) + 'px')
+            .style('top', (d3.event.pageY - 28) + 'px');
+        })
+        .on('mouseout', (d) => {
+          this.tooltip.transition()
+            .duration(300)
+            .style('opacity', 0);
+        })
+        .on('dblclick', function (d) {
+          d.fx = null;
+          d.fy = null;
+
+          d3.select(this).classed('fixed', false);
+        })
+        .call(d3.drag()
+          .on("start", function (d) {
+            if (!d3.event.active) self.simulation.alphaTarget(0.3).restart();
+            d.fx = d.x;
+            d.fy = d.y;
+          })
+          .on("drag", function (d) {
+            d.fx = d3.event.x;
+            d.fy = d3.event.y;
+          })
+          .on("end", function (d) {
+            if (!d3.event.active) self.simulation.alphaTarget(0);
+            // don't reset the fx and fy coordinates to have the node stick at that position
+            // d.fx = null;
+            // d.fy = null;
+
+            d3.select(this).classed('fixed', true);
+          })
+        );
+
+      // remove old nodes
+      node.exit().remove();
+
+      node = newNodes.merge(node);
+
+
+      this.simulation
+        .nodes(graph.nodes)
+        .on("tick", ticked);
+
+      this.simulation.force("link")
+        .links(graph.links);
+
+      ticked();
+
+      function ticked() {
+        link
+          .attr("x1", function (d) { return d.source.x; })
+          .attr("y1", function (d) { return d.source.y; })
+          .attr("x2", function (d) { return d.target.x; })
+          .attr("y2", function (d) { return d.target.y; });
+
+        node
+          .attr("cx", function (d) { return d.x; })
+          .attr("cy", function (d) { return d.y; });
+      }
+    });
+  }
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -27,11 +27,15 @@ export class Home {
     this.graphData = this.initialData;
   }
 
-  highlight(pattern) {
-    var regex = new RegExp(pattern, 'gi'); 
-    let nodes = this.initialData.nodes.map((node, idx) => {
-        if (pattern !== '' && node.id.indexOf(pattern) > -1) {
+  highlight(pattern, highlightArches) {
+    let nodesHighlighted = [];
+
+    try {
+      var regex = new RegExp(pattern, 'gi');
+      let nodes = this.graphData.nodes.map((node, idx) => {
+        if (pattern !== '' && regex.test(node.id)) {
           node.isHighlighted = true;
+          nodesHighlighted.push(node);
           console.log('highlighting', node);
         } else {
           node.isHighlighted = false;
@@ -40,53 +44,156 @@ export class Home {
         return node;
       });
 
-    this.graphData = {
-      nodes: nodes,
-      links: this.initialData.links
-    };
-    
-  }
+      let links;
+      let linksToRemove = [];
+      if (highlightArches && nodesHighlighted.length > 0) {
+        links = this.initialData.links.map((link, idx) => {
+          let idxPresent = false;
 
-  filterGraph(excludePattern) {
-    if (excludePattern && excludePattern !== '') {
+          nodesHighlighted.forEach((node) => {
+            if (node.id === link.target.id) {
+              link.highlighted = true;
+            } else {
+              if (!link.highlighted) {
+                link.highlighted = false;
+              }
+            }
+          });
 
-      let regex = /`${excludePattern}`/;
-      let removedNodeIdx = [];
-      let nodes = this.initialData.nodes.filter((node, idx) => {
-        if (excludePattern !== '' && node.id.indexOf(excludePattern) > -1) {
-          removedNodeIdx.push(node);
-          return false;
-        } else {
-          return true;
-        }
-      });
-
-      console.log('Nodes removed', removedNodeIdx);
-
-      let links = this.initialData.links.filter((link, idx) => {
-        let idxPresent = false;
-
-        removedNodeIdx.forEach((node) => {
-          if (node.id === link.source.id || node.id === link.target.id) {
-            idxPresent = true;
-          }
+          return link;
         });
 
-        if (idxPresent) {
-          console.log('Removing link', link);
-          return false;
-        } else {
-          return true;
-        }
-      });
+        // remove nodes
+        nodes = this.graphData.nodes.filter((node, idx) => {
+          links.forEach((link) => {
+            if (!link.highlighted && link.source.id === node.id || link.target.id === node.id) {
+              return false;
+            } else {
+              return true;
+            }
+          });
+        });
 
-      // set the new data
+        links = this.graphData.links.filter((link, idx) => {
+          return link.highlighted === false;
+        });
+
+
+
+      } else {
+        links = this.initialData.links;
+      }
+
       this.graphData = {
         nodes: nodes,
         links: links
       };
+    } catch (e) {
+      this.resetGraph();
+    }
+  }
 
-    } else {
+  isLinkHighlighted(links, nodeId) {
+
+  }
+
+  excludeGraph(excludePattern) {
+    try {
+      if (excludePattern && excludePattern !== '') {
+
+        let regex = new RegExp(excludePattern, 'gi');
+        console.log('regex: ', regex);
+        let removedNodeIdx = [];
+        let nodes = this.initialData.nodes.filter((node, idx) => {
+          if (excludePattern !== '' && regex.test(node.id)) {
+            removedNodeIdx.push(node);
+            return false;
+          } else {
+            return true;
+          }
+        });
+
+        console.log('Nodes removed', removedNodeIdx);
+
+        let links = this.initialData.links.filter((link, idx) => {
+          let idxPresent = false;
+
+          removedNodeIdx.forEach((node) => {
+            if (node.id === link.source.id || node.id === link.target.id) {
+              idxPresent = true;
+            }
+          });
+
+          if (idxPresent) {
+            console.log('Removing link', link);
+            return false;
+          } else {
+            return true;
+          }
+        });
+
+        // set the new data
+        this.graphData = {
+          nodes: nodes,
+          links: links
+        };
+
+      } else {
+        this.resetGraph();
+      }
+
+    } catch (e) {
+      this.resetGraph();
+    }
+
+  }
+
+  filterGraph(excludePattern) {
+    try {
+      if (excludePattern && excludePattern !== '') {
+
+        let regex = new RegExp(excludePattern, 'gi');
+        console.log('regex: ', regex);
+        let removedNodeIdx = [];
+        let nodes = this.initialData.nodes.filter((node, idx) => {
+          if (excludePattern !== '' && !regex.test(node.id)) {
+            removedNodeIdx.push(node);
+            return false;
+          } else {
+            return true;
+          }
+        });
+
+        console.log('Nodes removed', removedNodeIdx);
+
+        let links = this.initialData.links.filter((link, idx) => {
+          let idxPresent = false;
+
+          removedNodeIdx.forEach((node) => {
+            if (node.id === link.source.id || node.id === link.target.id) {
+              idxPresent = true;
+            }
+          });
+
+          if (idxPresent) {
+            console.log('Removing link', link);
+            return false;
+          } else {
+            return true;
+          }
+        });
+
+        // set the new data
+        this.graphData = {
+          nodes: nodes,
+          links: links
+        };
+
+      } else {
+        this.resetGraph();
+      }
+
+    } catch (e) {
       this.resetGraph();
     }
 
